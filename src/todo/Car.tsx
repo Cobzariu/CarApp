13bc7ad405a884ba46d8d65eeb034b3608b8ec88
Cobzariu <62259244+Cobzariu@users.noreply.github.com>
2020-12-12 18:43:34 +0200

    Add photo to list

--- src/todo/Car.tsx
+++ src/todo/Car.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { IonItem, IonLabel } from '@ionic/react';
-import { CarProps } from './CarProps';
+import React from "react";
+import { IonItem, IonLabel } from "@ionic/react";
+import { CarProps } from "./CarProps";
 
 interface CarPropsExt extends CarProps {
   onEdit: (_id?: string) => void;
 }
 
-const Item: React.FC<CarPropsExt> = ({ _id, name, onEdit }) => {
+const Item: React.FC<CarPropsExt> = ({ _id, name, onEdit, photoPath }) => {
   return (
     <IonItem onClick={() => onEdit(_id)}>
       <IonLabel>{name}</IonLabel>
+      <img src={photoPath} style={{ height: 50 }} />
     </IonItem>
   );
 };
